03ffb124dbdaceeff6fab1ab60f33847f5083c81
KB Benton <kbenton@yitr.com>
2016-04-19 21:55:37 -0500

    Fix bug - forgot to exit if authentication failed.

--- ajaxTemplate.php
+++ ajaxTemplate.php
@@ -30,12 +30,4 @@ if ( ! $auth->isAuthorized() ) {
     exit ;
 }
 
-// Sample call ajaxSaveApplicationStatusRow.php
-//                 ?id=1
-//                 &statusValue=FOUND
-//                 &style=background-color%3A%20lightgreen%3B%20color%3A%20blue%3B
-//                 &isActive=1
-//                 &sortKey=12
-
-$result = array( 'result' => 'Not implemented.' ) ;
-print json_encode( $result ) ;
+// New code goes here.
